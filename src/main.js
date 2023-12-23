7230323c21249ae3bc00b4ec14b505c4906e3c81
Anthony Gambale <u6867396@anu.edu.au>
2023-12-24 01:14:22 +1100

    nevermind it works now

--- src/main.js
+++ src/main.js
@@ -4,7 +4,5 @@ import { createApp } from 'vue'
 import { inject } from '@vercel/analytics';
 import App from './App.vue'
 
-console.log('about to run inject')
 inject();
-console.log('just ran inject')
 createApp(App).mount('#app')
