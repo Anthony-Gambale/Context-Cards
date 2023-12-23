35eb5789cc1bd522d5e514e1bbff27b8c5131b86
Anthony Gambale <u6867396@anu.edu.au>
2023-12-24 01:09:22 +1100

    sanity check

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import { createApp } from 'vue'
 import { inject } from '@vercel/analytics';
 import App from './App.vue'
 
+console.log('about to run inject')
 inject();
+console.log('just ran inject')
 createApp(App).mount('#app')
