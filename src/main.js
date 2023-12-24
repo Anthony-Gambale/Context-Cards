fe0019911b9e0073c5d819dd218f7124067d9e27
Anthony Gambale <u6867396@anu.edu.au>
2023-12-24 16:44:06 +1100

    fixed router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import { createApp } from 'vue';
 import { inject } from '@vercel/analytics';
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'; // Updated import
 
 inject();
-createApp(App).mount('#app')
+
+// Import your component(s) here
+import App from './App.vue';
+
+// Use VueRouter by installing it into your app instance
+const app = createApp(App);
+app.use(createRouter({
+  history: createWebHistory(),
+  routes: [] // Add your routes here
+})); 
+
+app.mount('#app');
