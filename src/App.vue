d0d9fea2a6fc5c6f26f8a1903e99a8084c1f1ed5
Anthony Gambale <u6867396@anu.edu.au>
2023-12-29 14:54:16 +1100

    fixed removal bug

--- src/App.vue
+++ src/App.vue
@@ -123,7 +123,10 @@ export default {
       return this.ankiExportPile
     },
     removeFromAnkiExportPile(removeCard) {
-      this.ankiExportPile = this.ankiExportPile.filter(card => card !== removeCard)
+      this.ankiExportPile = this.ankiExportPile.filter(card => {
+        return card.english != removeCard.english ||
+          card.targetLanguage != removeCard.targetLanguage
+      })
     },
     addToDeck(card) {
       this.deck[0].push(card) // add new cards to the *bottom* of the 0th pile
@@ -185,7 +188,10 @@ export default {
     },
     removeFromDeck(removeCard) {
       for (let x = 0; x < this.deck.length; x++) {
-        this.deck[x] = this.deck[x].filter(card => card !== removeCard)
+        this.deck[x] = this.deck[x].filter(card => {
+          return card.english != removeCard.english ||
+            card.targetLanguage != removeCard.targetLanguage
+        })
       }
       this.persistDeck()
     },
--- src/components/BrowseDeck.vue
+++ src/components/BrowseDeck.vue
@@ -9,8 +9,9 @@
   <div class="input-group mb-3">
     <input type="text" class="form-control" :placeholder="searchBarText" aria-label="Text input with clear button"
       aria-describedby="button-addon2" v-model="searchText" @keydown.enter="searchButtonClicked">
-    <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="searchButtonClicked">{{ buttonText
-    }}</button>
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="searchButtonClicked">
+      {{ buttonText }}
+    </button>
   </div>
 
   <p v-if="isEmpty()">
@@ -23,7 +24,7 @@
 
   <div v-for="(pile, i) in readDeckWrapper()" :key="i">
     <h4 v-if="pile.length > 0">Memory Level {{ i }}</h4>
-    <BrowseCard v-for="(card, j) in pile" :key="j" :card="card" :pile="i" :remove="removeFromDeck" />
+    <BrowseCard v-for="(card, j) in pile" :key="j" :card="card" :pile="i" :remove="removeFromDeckWrapper" />
   </div>
 </template>
 
@@ -56,10 +57,8 @@ export default defineComponent({
   },
   methods: {
     searchButtonClicked() {
-      if (this.searchText == "") {
-        this.currentDeckDisplay = this.readDeck()
-      } else {
-        this.currentDeckDisplay = this.readDeck()
+      this.currentDeckDisplay = this.readDeck()
+      if (this.searchText != "") {
         const options = {
           includeScore: true,
           keys: ['english', 'targetLanguage']
@@ -77,6 +76,18 @@ export default defineComponent({
     },
     readDeckWrapper() {
       return this.currentDeckDisplay
+    },
+    removeFromDisplay(removeCard) {
+      for (let x = 0; x < this.currentDeckDisplay.length; x++) {
+        this.currentDeckDisplay[x] = this.currentDeckDisplay[x].filter(card => {
+          return card.english != removeCard.english ||
+            card.targetLanguage != removeCard.targetLanguage
+        })
+      }
+    },
+    removeFromDeckWrapper(removeCard) {
+      this.removeFromDeck(removeCard)
+      this.removeFromDisplay(removeCard)
     }
   }
 })
